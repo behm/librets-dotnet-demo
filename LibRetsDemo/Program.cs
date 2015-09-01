dbb1e909441c02190e3ee29d6a2dde270bdf1e44
bbehm <brian@behm.net>
2015-09-01 10:44:53 -0500

    Shell of a librets app

---
+++ LibRetsDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using librets;
+
+namespace LibRetsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loginUrl = "";
+            var username = "";
+            var password = "";
+            var userAgent = "";
+
+            using (var retsSession = new RetsSession(loginUrl))
+            {
+                retsSession.SetUserAgent(userAgent);
+                retsSession.SetRetsVersion(RetsVersion.RETS_1_7_2);
+
+                if (!retsSession.Login(username, password))
+                {
+                    throw new Exception("RETS login failed");
+                }
+
+                var loginResponse = retsSession.GetLoginResponse();
+
+                // todo: inspect login response
+            }
+        }
+    }
+}
