9ceacad4a33cb94e1ca94e3f99eff5d3b9d0ab6a
behm <brian@behm.net>
2017-01-25 23:24:35 -0600

    Added work-around for https login URLs

--- LibRetsDemo/Program.cs
+++ LibRetsDemo/Program.cs
@@ -11,18 +11,25 @@ namespace LibRetsDemo
     {
         static void Main(string[] args)
         {
-            var loginUrl = "";
+            var loginUrl = "https://some.rets.server.com/rets/login";
             var username = "";
             var password = "";
             var userAgent = "";
 
             using (var retsSession = new RetsSession(loginUrl))
             {
+				var uri = new Uri(loginUrl);
+	            if (uri.Scheme == Uri.UriSchemeHttps)
+	            {
+					// ssl certificate work around - https://groups.google.com/forum/#!searchin/librets/ssl%7Csort:relevance/librets/g62O8pxsAcg/AXzlKi133TUJ
+					retsSession.SetModeFlags(RetsSession.MODE_NO_SSL_VERIFY);
+	            }
+
                 if (!string.IsNullOrEmpty(userAgent))
                 {
                     retsSession.SetUserAgent(userAgent);
                 }
-                retsSession.SetRetsVersion(RetsVersion.RETS_1_7_2);
+                retsSession.SetRetsVersion(RetsVersion.RETS_1_8_0);
 
                 if (!retsSession.Login(username, password))
                 {
